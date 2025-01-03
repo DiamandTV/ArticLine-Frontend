b165a93d0cb52b8d8baa414c04c9ad6b2fa1e797
rehmanCTA <Sn.rehman25@yahoo.com>
2025-01-03 23:02:50 +0100

    Added new features

--- src/models/auth.ts
+++ src/models/auth.ts
@@ -5,5 +5,5 @@ export interface AuthModel {
     phone_number:string,
     is_verified?:boolean,
     is_superuser?:boolean, 
-    type?:'USER' | 'COMPANY'
+    type?:'USER' | 'COMPANY' | 'COURIER'
 }
