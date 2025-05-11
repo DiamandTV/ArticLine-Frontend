b4c9a8f22c0525f199677e05be455c6090751789
rehmanCTA <Sn.rehman25@yahoo.com>
2025-05-11 16:53:47 +0200

    ...

--- src/features/home/pages/HomePage.tsx
+++ src/features/home/pages/HomePage.tsx
@@ -1,11 +1,15 @@
 
 import { PaddingView } from "@views/PaddingView";
 import { CategoryList } from "../components/lists/Category/CategoryList";
+import { StoreHomeList } from "@features/store/components/list/StoreList";
 
 export function HomePage() {
     return (
-        <PaddingView>
+        <PaddingView className="flex flex-col gap-2">
             <CategoryList />
+            <div className="w-full">
+                <StoreHomeList/>
+            </div>
         </PaddingView>
     );
 }
--- src/features/store/components/list/StoreList.tsx
+++ src/features/store/components/list/StoreList.tsx
@@ -3,6 +3,7 @@ import { useGetBusinessStoreListQuery } from "@features/store/hooks/useGetBusine
 import { StoreBusinessCard } from "../cards/StoreCard/StoreCard"
 import { useNavigate, useParams } from "react-router"
 import { getKey } from "@lib/kegGenerator/keyGenerator"
+import { useStoreHomeStoreListQuery } from "@features/store/hooks/useGetHomeStoreListQuery/useGetHomeStoreListQuery"
 
 export function StoreBusinessList(){
     const params = useParams()
@@ -29,6 +30,31 @@ export function StoreBusinessList(){
         </div>
     )
 }
+
+
+export function StoreHomeList(){
+    const navigator = useNavigate()
+    const {isLoading,isSuccess,data,ref} = useStoreHomeStoreListQuery()
+    if(isLoading || !isSuccess) return null
+    console.log(data)
+    return(
+        <div>
+            {data.map((store)=>{
+                return(
+                    <StoreProvider store={store}>
+                        <StoreBusinessCard  
+                            key={getKey()}
+                            onClick={()=>{
+                                navigator(`/company/${store.company_profile}/store/${store.id}/`)
+                            }}/>
+                    </StoreProvider>
+                )
+            })}
+            <div ref={ref} className="py-1"/>
+        </div>
+    )
+}
+
 
 // export function StoreList(){
 //     const {isLoading,isSuccess,data,ref} = useGetBusinessStoreListQuery({
